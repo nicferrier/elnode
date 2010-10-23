9756913340c989cd30e064690504c2fffb1a9b90
nferrier <none@none>
2010-10-23 23:54:16 +0100

    elisp documentation standards and the licence.

--- elnode.el
+++ elnode.el
@@ -1,13 +1,29 @@
-;;; elnode.el --- A simple emacs async http server
-;;; Copyright (C) 2010 by Nic Ferrier
+;;; elnode.el --- a simple emacs async HTTP server
+
+;; Copyright (C) 2010  Nic Ferrier
 
 ;; Author: Nic Ferrier <nferrier+elnode@ferrier.me.uk>
 ;; Maintainer: Nic Ferrier <nferrier+elnode@ferrier.me.uk>
 ;; Created: 5th October 2010
 ;; Version: 0.1
-;; Keywords: http webserver httpd asynchronous network server node.js
+;; Keywords: lisp
 
-;;; Commentary
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
 ;;
 ;; This is an elisp version of the popular node.js asyncrhonous
 ;; webserver toolkit.
