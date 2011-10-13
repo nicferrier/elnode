c33b142b0c52b3a4d4c5d6ab6e26a519bde8e236
Nic Ferrier <nic@ferrier.me.uk>
2011-10-13 22:40:22 +0100

    version 0.3

--- elnode.el
+++ elnode.el
@@ -5,7 +5,7 @@
 ;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Maintainer: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Created: 5th October 2010
-;; Version: 0.2
+;; Version: 0.3
 ;; Keywords: lisp, http
 
 ;; This file is NOT part of GNU Emacs.
