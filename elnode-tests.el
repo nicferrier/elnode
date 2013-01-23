1224f00b24060f6c719dbc5e9ee981fc81e162f1
Nic Ferier <nic@ferrier.me.uk>
2013-01-23 15:18:38 +0000

    remove a load of boilerplate from the elnode-tests

--- elnode-tests.el
+++ elnode-tests.el
@@ -1,39 +1,5 @@
 ;;; elnode-tests.el --- tests for Elnode -*- lexical-binding: t -*-
 
-;; Copyright (C) 2010, 2011, 2012  Nic Ferrier
-
-;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
-;; Maintainer: Nic Ferrier <nferrier@ferrier.me.uk>
-;; Created: 5th October 2010
-;; Keywords: lisp, http, hypermedia
-
-;; This file is NOT part of GNU Emacs.
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Commentary:
-;;
-;; This is just the tests for Elnode.
-
-;;; Style note
-;;
-;; This codes uses the Emacs style of:
-;;
-;;    elnode--private-function
-;;
-;; for private functions.
-
 ;;; Code:
 
 (require 'ert)
