a1dda9959134a195bab6f057eeff9e50802b452f
Nic Ferier <nic@ferrier.me.uk>
2013-02-03 05:22:19 +0000

    booting a server with elpakit demo

---
+++ examples/elpakit-boot.el
@@ -0,0 +1,17 @@
+;;; Booting elnode in a daemon
+
+;; A demonstration of using elpakit to start elnode in a separate
+;; daemon server.
+
+;; Elpakit builds packages and does things with them, puts them into
+;; local archives, evals them, runs tests on them.
+
+(elpakit-start-server
+ (list ;; A single element list with the repo in it
+  (file-name-directory
+   (directory-file-name
+    (file-name-directory
+     (buffer-file-name))))
+ 'elnode)
+
+;;; elpakit-boot.el ends here.
